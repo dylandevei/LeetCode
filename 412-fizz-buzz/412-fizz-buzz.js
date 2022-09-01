19b46daa8fcd7aa9f763e60f605e38b5411ea068
Dylan De Vei <dylan.devei@gmail.com>
2022-08-31 19:06:25 -0700

    Time: 102 ms (48.77%), Space: 44.2 MB (73.67%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -5,7 +5,7 @@
 var fizzBuzz = function(n) {
     let result = []
     for (let i = 1; i <= n; i++){
-        if (i % 3 === 0 && i % 5 ===0){
+        if (i % 3 === 0 && i % 5 === 0){
             result.push('FizzBuzz')
         } else if (i % 3 === 0){
             result.push('Fizz')
